df4247624cbc20020f620f0c55f504afbe5f65f7
extasy44 <seoheejun@gmail.com>
2021-03-10 16:25:36 +1100

    initial set up, restaurant screen, card components added

---
+++ note.js
@@ -0,0 +1,1 @@
+[{"content":"\n\nconst Restaurant = () => (\n  <div>\n    Hello! Restaurant!\n  </div>\n);\n\nshow(Restaurant)","type":"code","id":"ybvh7"},{"content":"","type":"code","id":"plt33"}]
