2765cdbe9d13347c4de9b6d51dd63eb0c2ba889b
extasy44 <seoheejun@gmail.com>
2021-03-24 14:38:33 +1100

    added tab navigation/bottom-nav

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import styled, { useTheme } from 'styled-components/native';
+import styled from 'styled-components/native';
 
 const sizeVariant = {
   small: 1,
   medium: 2,
-  large: 3,
-  xLarge: 4
+  large: 3
 };
 
 const positionVariant = {
@@ -23,15 +21,9 @@ const getVariant = (position, size, theme) => {
   return `${property}:${value}`;
 };
 
-const SpacerView = styled.View`
-  ${({ variant }) => variant};
+export const Spacer = styled.View`
+  ${({ position, size, theme }) => getVariant(position, size, theme)}
 `;
-
-export const Spacer = ({ position, size, children }) => {
-  const theme = useTheme();
-  const variant = getVariant(position, size, theme);
-  return <SpacerView variant={variant}>{children}</SpacerView>;
-};
 
 Spacer.defaultProps = {
   position: 'top',
